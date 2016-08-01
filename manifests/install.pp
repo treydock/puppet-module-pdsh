0b63fb773465f339335a88a30c2f4788e70a73f9
Trey Dockendorf <tdockendorf@osc.edu>
2016-08-01 12:12:02 -0400

    Add dependency on EPEL for RedHat based systems

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,26 +4,47 @@ class pdsh::install {
     fail("Use of private class ${name} by ${caller_module_name}")
   }
 
+  case $::osfamily {
+    'RedHat': {
+      include ::epel
+
+      $_package_require  = Yumrepo['epel']
+      $_package_defaults = {
+        'ensure'  => $pdsh::package_ensure,
+        'require' => $_package_require,
+      }
+    }
+    default: {
+      $_package_defaults = {
+        'ensure'  => $pdsh::package_ensure,
+      }
+    }
+  }
+
   package { 'pdsh':
-    ensure => $pdsh::package_ensure,
-    name   => $pdsh::package_name,
+    ensure  => $pdsh::package_ensure,
+    name    => $pdsh::package_name,
+    require => $_package_require,
   }
 
   package { 'pdsh-rcmd-rsh':
-    ensure => $pdsh::_rsh_package_ensure,
-    name   => $pdsh::rsh_package_name,
+    ensure  => $pdsh::_rsh_package_ensure,
+    name    => $pdsh::rsh_package_name,
+    require => $_package_require,
   }
 
   package { 'pdsh-mod-dshgroup':
-    ensure => $pdsh::package_ensure,
-    name   => $pdsh::dshgroup_package_name,
+    ensure  => $pdsh::package_ensure,
+    name    => $pdsh::dshgroup_package_name,
+    require => $_package_require,
   }
 
   package { 'pdsh-rcmd-ssh':
-    ensure => $pdsh::_ssh_package_ensure,
-    name   => $pdsh::ssh_package_name,
+    ensure  => $pdsh::_ssh_package_ensure,
+    name    => $pdsh::ssh_package_name,
+    require => $_package_require,
   }
 
-  ensure_packages($pdsh::extra_packages, { 'ensure' => $pdsh::package_ensure })
+  ensure_packages($pdsh::extra_packages, $_package_defaults)
 
 }
--- spec/shared_examples/install.rb
+++ spec/shared_examples/install.rb
@@ -1,8 +1,9 @@
 shared_context "pdsh::install" do
   it do
     is_expected.to contain_package('pdsh').only_with({
-      :ensure => 'present',
-      :name   => 'pdsh',
+      :ensure   => 'present',
+      :name     => 'pdsh',
+      :require  => 'Yumrepo[epel]'
     })
   end
 end

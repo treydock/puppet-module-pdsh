db3b7470c8fde63871c582fe9be7409c7e5bc6f5
Trey Dockendorf <tdockendorf@osc.edu>
2016-08-23 17:16:13 +0000

    Better support for clustershell and pdsh over SSH.  Moving away from rsh as rsh with pdsh requires setuid on pdsh for non-root to run commands.
    What will change is pdsh will run using rcmd ssh and some SSH options to disable checking user known hosts, disable writing out known hosts and disable printing known hosts messages.  If the environment variables in /etc/profile.d become a problem then the rsh package for pdsh will have to be removed.
    
    Detailed changes:
    
        Set clustershell to use specific SSH options to disable user known hosts checking
        Set pdsh to use ssh as default rcmd and use same SSH options as clustershell
        Change clustershell module to default to ssh_user undefined and ssh_path as ssh - this conforms to clush.conf documented defaults
        Allow pdsh module to set setuid bit on pdsh and pdcp
        Manage /etc/profile.d/pdsh.sh and /etc/profile.d/pdsh.csh
    
    git-svn-id: https://svn.osc.edu/repos/osc-puppetmaster-conf/trunk@796 60ccb607-682e-4238-bebe-9d451482b7c3

--- spec/shared_examples/config.rb
+++ spec/shared_examples/config.rb
@@ -18,4 +18,77 @@ shared_context "pdsh::config" do
       :mode    => '0755',
     })
   end
+
+  it do
+    is_expected.to contain_file('/usr/bin/pdsh').with({
+      :ensure => 'present',
+      :owner  => 'root',
+      :group  => 'root',
+      :mode   => '0755',
+    })
+  end
+
+  it do
+    is_expected.to contain_file('/usr/bin/pdcp').with({
+      :ensure => 'present',
+      :owner  => 'root',
+      :group  => 'root',
+      :mode   => '0755',
+    })
+  end
+
+  it do
+    is_expected.to contain_file('/etc/profile.d/pdsh.sh').with({
+      :ensure => 'absent',
+      :owner  => 'root',
+      :group  => 'root',
+      :mode   => '0644',
+    })
+  end
+
+  it do
+    is_expected.to contain_file('/etc/profile.d/pdsh.csh').with({
+      :ensure => 'absent',
+      :owner  => 'root',
+      :group  => 'root',
+      :mode   => '0644',
+    })
+  end
+
+  context 'when use_setuid is true' do
+    let(:params) {{ :use_setuid => true }}
+
+    it { is_expected.to contain_file('/usr/bin/pdsh').with_mode('4755') }
+    it { is_expected.to contain_file('/usr/bin/pdcp').with_mode('4755') }
+  end
+
+  context 'when rcmd_type defined' do
+    let(:params) {{ :rcmd_type => 'ssh' }}
+
+    it { is_expected.to contain_file('/etc/profile.d/pdsh.sh').with_ensure('file') }
+    it { is_expected.to contain_file('/etc/profile.d/pdsh.csh').with_ensure('file') }
+
+    it do
+      verify_contents(catalogue, '/etc/profile.d/pdsh.sh', ['export PDSH_RCMD_TYPE="ssh"'])
+    end
+
+    it do
+      verify_contents(catalogue, '/etc/profile.d/pdsh.csh', ['setenv PDSH_RCMD_TYPE "ssh"'])
+    end
+  end
+
+  context 'when ssh_args_append defined' do
+    let(:params) {{ :ssh_args_append => '-oStrictHostKeyChecking=no' }}
+
+    it { is_expected.to contain_file('/etc/profile.d/pdsh.sh').with_ensure('file') }
+    it { is_expected.to contain_file('/etc/profile.d/pdsh.csh').with_ensure('file') }
+
+    it do
+      verify_contents(catalogue, '/etc/profile.d/pdsh.sh', ['export PDSH_SSH_ARGS_APPEND="-oStrictHostKeyChecking=no"'])
+    end
+
+    it do
+      verify_contents(catalogue, '/etc/profile.d/pdsh.csh', ['setenv PDSH_SSH_ARGS_APPEND "-oStrictHostKeyChecking=no"'])
+    end
+  end
 end

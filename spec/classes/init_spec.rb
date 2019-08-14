95b4cdbb68b8ea78c04fd4cebad16e9a8a6ce15a
Trey Dockendorf <tdockendorf@osc.edu>
2019-08-14 15:19:16 -0400

    Use module Hiera data

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -15,7 +15,6 @@ describe 'pdsh' do
       it { is_expected.to compile.with_all_deps }
 
       it { is_expected.to create_class('pdsh') }
-      it { is_expected.to contain_class('pdsh::params') }
 
       it { is_expected.to contain_class('pdsh::install').that_comes_before('Class[pdsh::config]') }
       it { is_expected.to contain_class('pdsh::config') }
